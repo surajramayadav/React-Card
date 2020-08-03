b456424e4b154608bf731600986e19fdcf45c40a
Suraj yadav <58145626+surajramayadav@users.noreply.github.com>
2020-08-03 21:02:47 +0530

    2nd commit

---
+++ src/card.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+ 
+const Card=()=>{
+    return(
+        <> 
+        <div class="card">
+        <img src="https://images.pexels.com/photos/227417/pexels-photo-227417.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 " class="card-img-top" alt="hdsgdshb"/>
+       <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        
+       </div>
+       </div>
+       </>
+    );
+}
+export default Card;
